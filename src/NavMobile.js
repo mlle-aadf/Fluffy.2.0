5e70a571c29976913747299f3ac46b58774b267e
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2024-07-16 18:18:41 -0400

    layout + collapse/expand

---
+++ src/NavMobile.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { IoCloseOutline, IoMenu } from "react-icons/io5";
+import { useState } from "react";
+
+
+const NavMobile = () => {
+    // const links = [
+    //     {text: "HOME", linkTo: ""}
+    // ]
+
+    const [expanded, setExpanded] = useState(false)
+    const expandMenu = () => {
+        setExpanded(!expanded)
+    }
+    
+    return (
+        <>
+            <MenuIcon onClick={expandMenu} style={{display:`${expanded === false ? "block" : "none"}`}}/>
+            <Container style={{display:`${expanded === false ? "none" : "block"}`}}>
+                <CloseIcon onClick={expandMenu}/>
+                <Text>HOME</Text>
+                <Text>ABOUT</Text>
+                <Text>CONTACT</Text>
+                <Text>FR</Text>
+            </Container>
+        </>
+    );
+};
+
+export default NavMobile;
+
+const MenuIcon = styled(IoMenu)`
+    font-size: 2.5rem;
+    position: fixed;
+    top: 1rem;
+    right: 1.5rem;
+`
+
+const Container = styled.div`
+    background-color: var(--lightPink);
+    color: var(--darkPink);
+    width: 60%;
+    height: 100vh;
+    float: right;
+    text-align: right;
+    padding-right: 2rem;
+`
+
+const Text = styled.h1`
+    font-size: 1.75rem;
+    font-weight: 100;
+    margin: 1.5rem 0;
+    color: var(--darkPink);
+    `
+
+const CloseIcon = styled(IoCloseOutline)`
+    font-size: 2.5rem;
+    margin-top: 2rem;
+    color: var(--darkGreen)
+`
