3e08cfcd5c4f8d4c3fa67327277cbf1b41db3415
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-04-15 02:38:02 -0400

    mobile menu layout

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -4,111 +4,182 @@ import { IoCloseOutline } from "react-icons/io5";
 import { Link } from "react-scroll";
 import styled from "styled-components";
 import burgerMenu from "../assets/burger-menu.png";
+import IgIcon from "../assets/igIcon.png";
+import TikTokIcon from "../assets/tiktokIcon.png"; // Import the TikTok icon
 
 import { colors } from "../assets/data";
 import LanguageContext from "./LanguageContext";
 
 const NavMobile = () => {
+  const { language, texts, toggleLanguage } = useContext(LanguageContext);
+  const { nav } = texts[language];
 
-    const { language, texts, toggleLanguage } = useContext(LanguageContext)
-    const {nav} = texts[language] 
+  const [expanded, setExpanded] = useState(false);
 
-    const [expanded, setExpanded] = useState(false);
+  useEffect(() => {
+    const mobileNav = document.getElementById("mobileNav");
+    mobileNav.style.opacity = 0;
 
-    useEffect(() => {
-        const mobileNav = document.getElementById("mobileNav");
-        mobileNav.style.opacity = 0;
+    const handleScroll = () => {
+      const clientHeight = window.innerHeight;
+      const currentHeight = window.scrollY;
 
-        const handleScroll = () => {
-            const clientHeight = window.innerHeight;
-            const currentHeight = window.scrollY;
+      mobileNav.style.opacity = currentHeight >= clientHeight - 100 ? 1 : 0;
+    };
 
-            currentHeight >= clientHeight-100
-                ? (mobileNav.style.opacity = 1)
-                : (mobileNav.style.opacity = 0);
-        };
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    const toggleLanguageHandler = (isLanguageLink) => {
-        setExpanded(!expanded)
-        return isLanguageLink=== true ? toggleLanguage() : null
-    }
+  // Handle clicks outside the menu
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const container = document.getElementById("menuContainer");
+      if (expanded && container && !container.contains(event.target)) {
+        setExpanded(false);
+      }
+    };
 
-    return (
-        <>
-            <MenuIcon
-                id="mobileNav"
-                src={burgerMenu}
-                alt="Menu Icon"
-                onClick={() => setExpanded(!expanded)}
-            />
-            <Container expanded={expanded}>
-                <CloseIcon onClick={() => setExpanded(!expanded)} />
-                {nav.map((link, i) => {
-                    const isLanguageLink = i === 3;
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [expanded]);
 
-                    return (
-                        <MobileLink key={`navLink-${link}`} color={colors[i]}>
-                            <Link
-                                to={`section-${i}`}
-                                onClick={() => toggleLanguageHandler(isLanguageLink)}
-                            >
-                                {link}
-                            </Link>
-                        </MobileLink>
-                    );
-                })}
-            </Container>
-        </>
-    );
+  const toggleLanguageHandler = (isLanguageLink) => {
+    setExpanded(!expanded);
+    return isLanguageLink === true ? toggleLanguage() : null;
+  };
+
+  return (
+    <>
+      <MenuIcon
+        id="mobileNav"
+        src={burgerMenu}
+        alt="Menu Icon"
+        onClick={() => setExpanded(!expanded)}
+      />
+      <Container id="menuContainer" expanded={expanded}>
+        <CloseIcon onClick={() => setExpanded(!expanded)} />
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          {nav.map((link, i) => {
+            const isLanguageLink = i === 3;
+
+            return (
+              <MobileLink key={`navLink-${link}`} color={colors[i]}>
+                <Link
+                  to={`section-${i}`}
+                  onClick={() => toggleLanguageHandler(isLanguageLink)}
+                >
+                  {link}
+                </Link>
+              </MobileLink>
+            );
+          })}
+          <SocialIcons>
+            <IgLink
+              href="https://www.instagram.com/fluffysmtl/"
+              target="blank"
+              aria-label="Instagram"
+            >
+              <img src={IgIcon} alt="Instagram Icon" />
+            </IgLink>
+            <TikTokLink
+              href="https://www.tiktok.com/@fluffysmtl"
+              target="blank"
+              aria-label="TikTok"
+            >
+              <img src={TikTokIcon} alt="TikTok Icon" />
+            </TikTokLink>
+          </SocialIcons>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default NavMobile;
 
 const MenuIcon = styled.img`
-    cursor: pointer;
-    display: block;
-    height: 2.5rem;
-    position: fixed;
-    right: 2.5rem;
-    top: 2.5rem;
-    width: 2.5rem;
-    z-index: 2;
+  cursor: pointer;
+  display: block;
+  height: 2.5rem;
+  position: fixed;
+  right: 2.5rem;
+  top: 2.5rem;
+  width: 2.5rem;
+  z-index: 2;
 `;
 
 const Container = styled.div`
-    align-items: end;
-    background-color: rgba(0, 0, 0, 0.8);
-    flex-direction: column;
-    height: 100vh;
-    text-align: end;
-    padding-right: 2rem;
-    position: fixed;
-    right: 0;
-    width: 60%;
-    z-index: 3;
-    opacity: ${({ expanded }) => (expanded ? 1 : 0)};
-    visibility: ${({ expanded }) => (expanded ? "visible" : "hidden")};
-    transition: opacity 0.3s ease, visibility 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-end;
+  background-color: rgba(0, 0, 0, 0.8);
+  height: 100vh;
+  text-align: right;
+  padding: 0 2.5rem 0 0;
+  position: fixed;
+  right: 0;
+  width: 50%;
+  z-index: 3;
+  opacity: ${({ expanded }) => (expanded ? 1 : 0)};
+  visibility: ${({ expanded }) => (expanded ? "visible" : "hidden")};
+  transition: opacity 0.2s ease, visibility 0.2s ease;
 `;
 
 const MobileLink = styled.li`
-    color: var(--background-color);
-    font-family: 'Jost', sans-serif;
-    font-size: 1.75rem;
-    font-weight: 300;
-    letter-spacing: 0.15rem;
-    list-style-type: none;
-    margin: 1.5rem 0;
-    text-decoration: none;
+  color: var(--background-color);
+  font-family: "Jost", sans-serif;
+  font-size: 1.75rem;
+  font-weight: 400;
+  letter-spacing: 0.15rem;
+  list-style-type: none;
+  margin: 1.5rem 0;
+  text-decoration: none;
 `;
 
 const CloseIcon = styled(IoCloseOutline)`
-    color: white;
-    font-size: 2.5rem;
-    margin-top: 2rem;
+  color: white;
+  font-size: 3rem;
+  margin-top: 2rem;
+  margin-bottom: 3rem;
+`;
+
+const SocialIcons = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 20px;
+  margin: 3rem 0 0 auto;
+`;
+
+const IgLink = styled.a`
+  img {
+    width: 40px;
+    height: 40px;
+    transition: transform 0.3s;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+const TikTokLink = styled.a`
+  img {
+    width: 30px;
+    height: 30px;
+    transition: transform 0.3s;
+  }
 `;
